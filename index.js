7fdfca4efb8d1292aa4a60851542baf859d92402
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-03-30 22:32:34 -0500

    Finished choosing gesture function and class for both human and Ai. Tommorrow i will focus on incorperating them into the Game class

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 'use strict';
 
-const PromptSync = require('prompt-sync');
-
 const prompt = require('prompt-sync')();
 
 
 class Game {
     constructor() {
-        this.playerOne = new Player('Oscar');
-        this.playerTwo = new Player('John');
-        this.playerThree = new Player('AI');
-
-        
-
+        this.playerOne = new Human('Oscar');
+        this.playerTwo = new Ai('Jarvis');
     }
 
  
@@ -21,8 +15,8 @@ class Game {
         this.displayRules();
 
         while(this.playerOne.score < 3 && this.playerTwo < 3){
-            let playerOneTotal = this.playerOne.throwAllRpsls(this.rpsls);
-            let playerTwoTotal = this.playerTwo.throwAllRpsls(this.rpsls);
+            let playerOneTotal = this.playerOne.choosenGesture;
+            let playerTwoTotal = this.playerTwo.choosenGesture;
 
             if(playerOneTotal > playerTwoTotal){
                 console.log(this.playerOne.name + ' wins the round!');
@@ -36,6 +30,8 @@ class Game {
                 console.log('Wow! You managed to tie after each round which totals to ' + this.rpsls.length + ' !');
             }
         }
+
+        this.displayGameWinner();
     }
 
 
@@ -59,24 +55,6 @@ class Game {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 class Player{
@@ -84,75 +62,89 @@ class Player{
         this.score = 0;
         this.name = name;
         this.selectedGesture = null;
-
         this.rpsls = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     }
 }
 
 class Human extends Player{
     constructor(){
-        super()
+        super();
+        this.selectedGesture;
 
     }
 
-    chooseGesture(){
+    choosenGesture(){
         let choosenGesture = prompt('Please select gesture ');
 
         switch(choosenGesture){
             case 'rock':
                 this.selectedGesture = this.rpsls[0];
                 break;
+            case 'paper':
+                this.selectedGesture = this.rpsls[1];
+                break;
+            case 'scissors':
+                this.selectedGesture = this.rpsls[2];
+                break;
+            case 'lizard':
+                this.selectedGesture = this.rpsls[3];
+                break;
+            case 'spock':
+                this.selectedGesture = this.rpsls[4];
+                break;
         }
 
     }
 
 
-
 }
 
 
+class Ai extends Player{
+    constructor(){
+        super()
 
-class Rpsls {
-    constructor(rockPaperScissorsLizardSpock) {
-      this.rockPaperScissorsLizardSpock = rockPaperScissorsLizardSpock;
     }
 
     generateRandomNumber() {
-      let randomNumber = Math.floor(Math.random() * this.rockPaperScissorsLizardSpock) + 1;
-      return randomNumber;
+        let randomNumber = Math.floor(Math.random() * this.rockPaperScissorsLizardSpock) + 1;
+        return randomNumber;
+      }
+
+    choosenGesture(){
+        let choosenGesture;
+
+        switch(choosenGesture){
+            case 'rock':
+                this.selectedGesture = this.rpsls[this.generateRandomNumber]
+                break;
+            case 'paper':
+                this.selectedGesture = this.rpsls[this.generateRandomNumber];
+                break;
+            case 'scissors':
+                this.selectedGesture = this.rpsls[this.generateRandomNumber];
+                break;
+            case 'lizard':
+                this.selectedGesture = this.rpsls[this.generateRandomNumber];
+                break;
+            case 'spock':
+                this.selectedGesture = this.rpsls[this.generateRandomNumber];
+                break;
+        }
+
     }
+
+
 }
 
 
 
 
-function throwRpsls(rpsls) {
-    let throwResult = rpsls.generateRandomNumber();
-    return throwResult;
-  }
 
-function throwAllRpsls(rpslsArray) {
-    let runningTotal = 0;
-    if (rpslsArray == 'rock'){
-      return 0;
-      }
-  else if (rpslsArray == 'Spock'){
-      return 1;
-      }
-  else if (rpslsArray == 'paper'){
-      return 2;
-      }
-  else if (rpslsArray == 'lizard'){
-      return 3;
-      }
-  else if (rpslsArray == 'scissor'){
-      return 4;
-      }
 
-    for(let i = 0; i < rpslsArray.length; i++) {
-      let result = this.throwAllRpsls(rpslsArray[i]);
-      runningTotal += result;
-    }
 
-    return runningTotal;
-  }
+
+
+
+let game = new Game();
+game.runGame();
